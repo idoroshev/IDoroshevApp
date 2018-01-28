e53c7c7598f61931e239cc2543f31541968a3a5b
idoroshev <igdor9399@gmail.com>
2018-01-28 12:31:57 +0300

    Added plugins & token and app ids

--- scripts/deploy-hockey.sh
+++ scripts/deploy-hockey.sh
@@ -3,5 +3,5 @@ curl \
 -F "status=2" \
 -F "notify=0" \
 -F "ipa=@app/build/outputs/apk/release/app-release.apk" \
--H "X-HockeyAppToken: $HOCKEY_APP_TOKEN" \
-https://rink.hockeyapp.net/api/2/apps/$HOCKEY_APP_ID/app_versions/upload
+-H "X-HockeyAppToken: 0b72a455f87e4e478ba9e0a22c9a7681" \
+https://rink.hockeyapp.net/api/2/apps/84f0f25bd4974458b7a3c92f5d5bde05/app_versions/upload
